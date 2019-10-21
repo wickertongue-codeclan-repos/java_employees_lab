bdcb4d81fd1379c12947a697adbefc85fb80bb10
hayden <haydenwb@gmail.com>
2019-10-21 13:53:15 +0100

    created employee class, and employee interface

---
+++ src/main/java/com/codeclan/example/employee_tracking/EmployeeTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employee_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTrackingApplication.class, args);
+	}
+
+}
